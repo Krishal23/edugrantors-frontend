9aa64f92a7ac7908536f1b40ebfce969542f324f
Krishal23 <kanhakesharwani23@gmail.com>
2025-05-10 15:10:51 +0530

    upd

--- middleware/catchAsyncError.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const CatchAsyncErrror = (theFunc: any) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next)
-}
